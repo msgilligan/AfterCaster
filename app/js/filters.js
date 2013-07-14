8fd12e3252d822d34789d64a281acee91b6cb2f5
Sean Gilligan <sean@msgilligan.com>
2013-07-14 14:39:51 -0700

    Fix fluent-style define of second filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,9 +7,8 @@ angular.module('ac.filters', []).filter('hashtruncat',function(){
       var th = input.replace(/^0+/, '');
       return th.substring(0,12)+'...'+th.slice(-12);
     };
-  });
-
-angular.module('ac.filters', []).filter('exhashtruncat',function(){
+  }).
+  filter('exhashtruncat',function(){
     return function(input){
       var th = input.replace(/^0+/, '');
       return th.substring(0,5)+'...'+th.slice(-5);
