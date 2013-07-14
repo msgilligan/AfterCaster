441ddc351aad58ef36d910830624641417ea949a
Sean Gilligan <sean@msgilligan.com>
2013-07-14 14:11:08 -0700

    Use mocked JSON responses for now

--- app/js/services.js
+++ app/js/services.js
@@ -5,23 +5,28 @@
 angular.module('ac.services', ['ngResource']).
   value('version', '0.1').
   factory('Block', function($resource) {
-    var Block = $resource('http://www.corsproxy.com/blockchain.info/rawblock/:hash',
+//    var Block = $resource('http://www.corsproxy.com/blockchain.info/rawblock/:hash',
+    var Block = $resource('/app/rest/Block-get.json',
                         {}, {
                         'get': {method:'GET', cache: true},
-                        'query': {method: 'GET', url: 'http://www.corsproxy.com/blockchain.info/blocks/0?format=json', isArray:false, cache: true},
-                        'latest': {method: 'GET', url: 'http://www.corsproxy.com/blockchain.info/latestblock', isArray:false, cache: true}
+//                        'query': {method: 'GET', url: 'http://www.corsproxy.com/blockchain.info/blocks/0?format=json', isArray:false, cache: true},
+//                        'latest': {method: 'GET', url: 'http://www.corsproxy.com/blockchain.info/latestblock', isArray:false, cache: true}
+                        'query': {method: 'GET', url: '/app/rest/Block-query.json', isArray:false, cache: true},
+                        'latest': {method: 'GET', url: '/app/rest/Block-latest.json', isArray:false, cache: true}
                         });
     return Block;
   }).
   factory('Tx', function($resource) {
-    var Tx = $resource('http://www.corsproxy.com/blockchain.info/rawtx/:hash',
+//    var Tx = $resource('http://www.corsproxy.com/blockchain.info/rawtx/:hash',
+    var Tx = $resource('/app/rest/Tx-get.json',
                         {}, {
                         'get': {method:'GET', cache : true},
                         });
     return Tx;
   }).
   factory('Addr', function($resource) {
-    var Addr = $resource('http://www.corsproxy.com/blockchain.info/rawaddr/:addr',
+//    var Addr = $resource('http://www.corsproxy.com/blockchain.info/rawaddr/:addr',
+    var Addr = $resource('/app/rest/Addr-get.json',
                         {}, {
                         'get': {method:'GET', cache : true},
                         });
