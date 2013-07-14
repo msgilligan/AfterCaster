0934c2ed221259db5e0507560f3abf1e9e5dbae0
Mikhail Xu <mikhail.xu@gmail.com>
2013-07-14 14:23:58 -0700

    jesus christ 320 is small

--- app/js/filters.js
+++ app/js/filters.js
@@ -8,3 +8,10 @@ angular.module('ac.filters', []).filter('hashtruncat',function(){
       return th.substring(0,12)+'...'+th.slice(-12);
     };
   });
+
+angular.module('ac.filters', []).filter('exhashtruncat',function(){
+    return function(input){
+      var th = input.replace(/^0+/, '');
+      return th.substring(0,5)+'...'+th.slice(-5);
+    };
+  });
