0471eac4061075d862e4438fcdcc3002df707851
Sean Gilligan <sean@msgilligan.com>
2013-12-10 23:26:47 -0800

    Handle filter 'input' of 'undefined'

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,15 +2,24 @@
 
 /* Filters */
 
-angular.module('ac.filters', []).filter('hashtruncat',function(){
+angular.module('ac.filters', []).
+  filter('hashtruncat',function(){
     return function(input){
-      var th = input.replace(/^0+/, '');
-      return th.substring(0,12)+'...'+th.slice(-12);
+      var outval = "";
+      if (input != undefined) {
+        var th = input.replace(/^0+/, '');
+        outval = th.substring(0,12)+'...'+th.slice(-12);
+      }
+      return outval;
     };
   }).
   filter('exhashtruncat',function(){
     return function(input){
-      var th = input.replace(/^0+/, '');
-      return th.substring(0,5)+'...'+th.slice(-5);
+      var outval = "";
+      if (input != undefined) {
+        var th = input.replace(/^0+/, '');
+        outval = th.substring(0,5)+'...'+th.slice(-5);
+      }
+      return outval;
     };
   });
